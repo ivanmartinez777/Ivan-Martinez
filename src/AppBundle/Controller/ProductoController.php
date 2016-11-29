b362aa4f5e373f51f5ba67c23ff5b47507e17183
ivanmartinez777 <ivanmartinez777@hotmail.com>
2016-11-29 13:43:37 +0100

    compila

--- src/AppBundle/Controller/ProductoController.php
+++ src/AppBundle/Controller/ProductoController.php
@@ -13,28 +13,12 @@ class ProductoController extends Controller
     * @return \Symfony\Component\HttpFoundation\Response
     */
     public function indexAction()
-    {   /*$m = $this->getDoctrine()->getManager();
-        $p = new Product();
-        $p
-            ->setName('Meizu MX5')
-            ->setDescription('Chino con cierta garantía')
-            ->setPrice('300')
-            ;
-        $m->persist($p); Así se añaden clases*/
+    {
     $m = $this->getDoctrine()->getManager();
         $repo = $m->getRepository('AppBundle:Product');
-    //    $m->flush();
-       /* $p = $repo->findOneBy([
-            'name' => 'Meizu MX5',
-        ]);*/
        $products = $repo->findAll();
-        $p = $repo->find(10);
-        $p->setPrice('1100');
-        $p->setDescription('Black Frida- Que nos los quitan de las manos');
+        $p = $repo->find(13);
 
-        $p1 = $repo->findOneByName('Meizu MX5');
-
-        $m->remove($p1);
 
         $m->flush();
         return $this->render(':producto:index.html.twig',
